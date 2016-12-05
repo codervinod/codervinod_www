80e58da188584406a8cfee0e1fc4dbfffbf3e466
Vinod Gupta <vinod.gupta@nutanix.com>
2016-12-04 22:47:33 -0800

    Add email from contact form

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var send_email = require('../lib/send_email')
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  res.redirect('/#contact')
+	var name = req.body.name;
+	var email = req.body.email;
+	var subject = req.body.subject;
+	var message = req.body.message;
+	var email = {
+	   text:    message,
+	   from:    name + "<" + email + ">",
+	   to:      "Vinod Gupta <codervinod@gmail.com>",
+	   subject: subject
+	};
+	send_email.send(email, function(err, message){
+		console.log(err);
+		res.redirect('/#contact');
+	});
 });
 
 module.exports = router;
