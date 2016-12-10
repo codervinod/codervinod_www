4354f4fdbd91a5353ee8c39da72ce2e39463d2db
Vinod Gupta <vinod.gupta@nutanix.com>
2016-12-09 17:14:04 -0800

    Fix email address.

--- routes/contact.js
+++ routes/contact.js
@@ -8,9 +8,10 @@ router.post('/', function(req, res, next) {
 	var email = req.body.email;
 	var subject = req.body.subject;
 	var message = req.body.message;
+	var message = "name: " + name + "/nemail: " + email + "/n" + message;
 	var email = {
 	   text:    message,
-	   from:    name + "<" + email + ">",
+	   from:    codervinod.com + "<" + me@codervinod.com + ">",
 	   to:      "Vinod Gupta <codervinod@gmail.com>",
 	   subject: subject
 	};
